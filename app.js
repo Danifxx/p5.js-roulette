6211aff547b9f4b1b333998217b2df759b091cf4
unknown <tanidaig@yahoo.co.jp>
2020-01-13 23:19:56 +0900

    roulette works

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ var Mode = {
     acceleration: 1,
     constant: 2,
     deceleration: 3,
-    result: 4,
-    beforeSetting: 5
+    result: 4
 };
-var mode = Mode.beforeSetting;
+var mode = Mode.waiting;
 var nameList = [];
 var probabilityList = [];
+var colorList = [];
 var ratioSum = 0;
 var speed = 0.0;
 var theta = 0.0;
+var len = 0;
+var resultDisplayed = false;
 
 const ACCEL = 0.01;
 const DECEL = 0.01;
 const MAX_SPEED = 1.0;
+const RADIUS = 100;
+const COLOR_ADJ = 0.4;
+const TRIANGLE_SIZE = 10;
+const MARGIN = 10;
+const DECEL_RAND_LEVEL = 10;
+const DECEL_RAND_MAGNITUDE = 0.001;
 
 var holding = false;
 
@@ -34,6 +42,8 @@ function setup(){
     stroke(0,0,0);
     fill(0,0,0);
     background(255,255,255);
+    recalculate();
+    dataFetch();
 }
 
 function mousePressed(){
@@ -54,22 +64,83 @@ function touchEnded(){
     mouseReleased();
 }
 
+//set color indicator
+function cssColorSet(){
+    var counter = 0;
+    $('.color-indicator').each(function(){+
+        push();
+        colorMode(HSL, 255);
+        var c = color(colorList[counter],255-COLOR_ADJ*colorList[counter],128);
+        pop();
+        $(this).css('background-color', "rgb("+c._getRed()+","+c._getGreen()+","+c._getBlue()+")");
+        counter++;
+    });
+}
+
+//input to array
 function dataFetch(){
-    list = [];
+    ratioSum = 0.0;
     $('.item').each(function(){
         var ratio = $(this).find('.ratio').val()-0;
         ratioSum += ratio;
     });
+    nameList = [];
+    probabilityList = [];
     $('.item').each(function(){
         var name = $(this).find('.name').val();
         var ratio = $(this).find('.ratio').val()-0;
         nameList.push(name);
         probabilityList.push(ratio/ratioSum);
     });
+    //color
+    var colors = [];
+    len = nameList.length;
+    for(var i=0;i<len;i++){
+        colors.push(Math.floor(255/len*i));
+    }
+    colorList = [];
+    if(len%2==0){
+        for(var i=0;i<len;i+=2){
+            colorList[i] = colors[Math.floor(i/2)];
+        }
+        for(var i=1;i<len;i+=2){
+            colorList[i] = colors[Math.floor(i/2 + len/2)];
+        }
+    }else{
+        for(var i=0;i<len;i+=2){
+            colorList[i] = colors[Math.floor(i/2)];
+        }
+        for(var i=1;i<len;i+=2){
+            colorList[i] = colors[Math.floor(i/2)+Math.floor(len/2)+1];
+        }
+    }
+    cssColorSet();
+}
+
+function validation(){
+    var badflag = false;
+    $('.name').each(function(){
+        if($(this).val()==""){
+            badflag = true;
+        }
+    });
+    $('.ratio').each(function(){
+        if(!($(this).val()>0)){
+            badflag = true;
+        }
+    });
+    if(badflag){
+        alert('項目名と割合を正しく設定してください。');
+        return 1;
+    }
+    return 0;
 }
 
 function start(){
-    if(mode==Mode.beforeSetting||mode==Mode.waiting){
+    if(mode==Mode.waiting){
+        if(validation()==1){
+            return;
+        }
         $('#stop').css('display', 'inline-block');
         $('#start').css('display', 'none');
         dataFetch();
@@ -79,7 +150,7 @@ function start(){
 
 function stop(){
     if(mode==Mode.acceleration || mode==Mode.constant){
-        $('#start').css('display', 'inline-block');
+        $('#start').css('display', 'none');
         $('#stop').css('display', 'none');
         mode = Mode.deceleration;
     }
@@ -91,25 +162,40 @@ function reset(){
     theta = 0.0;
     speed = 0.0;
     mode = Mode.waiting;
+    if(validation()==0){
+        dataFetch();
+    }
+    $('#result').html('????');
+    resultDisplayed = false;
 }
 
-function drawAllShapes(){
-    fill(100,255,255);
-    arc(0,0,100,100,0,1*PI);
-
+function drawRoulette(){
+    var angleSum = 0.0;
+    push();
+    colorMode(HSL, 255);
+    for(var i=0;i<len;i++){
+        fill(colorList[i],255-COLOR_ADJ*colorList[i],128);
+        arc(0,0,RADIUS*2,RADIUS*2,angleSum,angleSum+2*PI*probabilityList[i]);
+        angleSum += probabilityList[i]*2*PI;
+    }
+    pop();
 }
 
 function draw(){
     fill(255,255,255);
     rect(0,0,width,height);
     translate(width/2, height/2);
-    switch(mode){
-    case Mode.beforeSetting:
 
-        break;
+    fill(255,0,0);
+    push();
+    translate(0, -RADIUS-MARGIN);
+    triangle(0, 0, -TRIANGLE_SIZE/2, -TRIANGLE_SIZE, TRIANGLE_SIZE/2, -TRIANGLE_SIZE);
+    pop();
+
+    switch(mode){
     case Mode.waiting:
         rotate(theta);
-        drawAllShapes();
+        drawRoulette();
         break;
     case Mode.acceleration:
         if(speed<MAX_SPEED){
@@ -118,31 +204,46 @@ function draw(){
             mode = Mode.constant;
         }
         theta += speed;
-        theta-=(floor(theta/2/PI))*2*PI;
+        theta-=(Math.floor(theta/2/PI))*2*PI;
         rotate(theta);
-        drawAllShapes();
+        drawRoulette();
         break;
     case Mode.constant:
         theta += speed;
-        theta-=(floor(theta/2/PI))*2*PI;
+        theta-=(Math.floor(theta/2/PI))*2*PI;
         rotate(theta);
-        drawAllShapes();
+        drawRoulette();
         break;
     case Mode.deceleration:
         if(speed>DECEL){
-            speed -= DECEL;
+            speed -= DECEL+getRandomInt(-DECEL_RAND_LEVEL,DECEL_RAND_LEVEL)*DECEL_RAND_MAGNITUDE;
         }else{
             speed = 0.0;
             mode = Mode.result;
         }
         theta += speed;
-        theta-=(floor(theta/2/PI))*2*PI;
+        theta-=(Math.floor(theta/2/PI))*2*PI;
         rotate(theta);
-        drawAllShapes();
+        drawRoulette();
         break;
     case Mode.result:
         rotate(theta);
-        drawAllShapes();
+        drawRoulette();
+        if(!resultDisplayed){
+            resultDisplayed = true;
+            var angleSum = theta;
+            var beforeAngleSum = theta;
+            var result = 0;
+            for(var i=0;i<len;i++){
+                angleSum += probabilityList[i]*2*PI;
+                if((angleSum>3/2*PI&&beforeAngleSum<3/2*PI) || (angleSum>7/2*PI&&beforeAngleSum<7/2*PI)){
+                    result = i;
+                    break;
+                }
+                beforeAngleSum = angleSum;
+            }
+            $('#result').html(nameList[result]);
+        }
         break;
     }
 }
